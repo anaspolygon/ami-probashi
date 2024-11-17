62b7b56ab6603fe3b1a3c9bd74a6c3c3ec50a4b8
Al Anas <al.anas@polygontech.xyz>
2024-11-17 12:27:54 +0600

    ⬆️  Update
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: 52345

--- src/app/career-consultancy.html/page.tsx
+++ src/app/career-consultancy.html/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="w-[1170px] mx-auto my-5">
+    <div className="w-[1170px] mx-auto my-5 px-4 md:px-0">
       <h2>Career Consultancy page</h2>
     </div>
   );
